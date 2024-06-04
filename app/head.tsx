5bf5e81376890ad86753b55a8b03d3ed090db818
Haris Khurshid Abbasi <abbasihariskhurshid@gmail.com>
2024-06-04 11:30:15 +0500

    message

--- app/head.tsx
+++ app/head.tsx
@@ -1,22 +1,22 @@
 export default function Head() {
-    return (
-        <>
-            <title>Adil Portfolio</title>
-            <meta name="description" content="Adil's Portfolio Website" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <script
-                data-name="BMC-Widget"
-                data-cfasync="false"
-                src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-                data-id="adilsherdotpro"
-                data-description="Support me on Buy me a coffee!"
-                data-message="Thank you for visiting! Support my coding channel here!"
-                data-color="#5F7FFF"
-                data-position="Right"
-                data-x_margin="18"
-                data-y_margin="18"
-                async
-            ></script>
-        </>
-    )
+  return (
+    <>
+      <title>Haris Portfolio</title>
+      <meta name="description" content="Haris Portfolio Website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="adilsherdotpro"
+        data-description="Support me on Buy me a coffee!"
+        data-message="Thank you for visiting! Support my coding channel here!"
+        data-color="#5F7FFF"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+        async
+      ></script>
+    </>
+  );
 }
